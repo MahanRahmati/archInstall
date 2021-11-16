4e7089ed8d6868fcb020b7363589f6fb158f7bf3
MahanRahmati <mahan.rahmati@protonmail.com>
2021-11-16 16:37:39 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,15 +39,13 @@ select ENTRY in $(lsblk -dpnoNAME | grep -P '/dev/sd|nvme|vd'); do
 	DISK3="${DISK}3"
 	break
 done
-swapon -s | grep $DISK2 >/dev/null
-if [ $? == 0 ]; then
-	swapoff $DISK2
+if swapon -s | grep "$DISK2" >/dev/null; then
+	swapoff "$DISK2"
 fi
-mountpoint -q /mnt
-if [ $? == 0 ]; then
+if mountpoint -q /mnt; then
 	umount -A --recursive /mnt
 fi
-wipefs -a -f $DISK
+wipefs -a -f "$DISK"
 (
 	echo n
 	echo p
@@ -65,20 +63,20 @@ wipefs -a -f $DISK
 	echo
 	echo
 	echo w
-) | fdisk $DISK -w always -W always
+) | fdisk "$DISK" -w always -W always
 
 echo "--------------------------------------------------"
 echo -e '\e[32m[3] Format the partitions\e[39m'
 echo "--------------------------------------------------"
 if [ "$BIOS_TYPE" == "uefi" ]; then
-	mkfs.fat -F32 $DISK1
+	mkfs.fat -F32 "$DISK1"
 fi
 if [ "$BIOS_TYPE" == "bios" ]; then
-	mkfs.ext4 $DISK1
+	mkfs.ext4 "$DISK1"
 fi
-mkswap $DISK2
-mkfs.btrfs $DISK3
-mount $DISK3 /mnt
+mkswap "$DISK2"
+mkfs.btrfs "$DISK3"
+mount "$DISK3" /mnt
 cd /mnt
 btrfs subvolume create root
 btrfs subvolume create home
@@ -91,23 +89,23 @@ umount /mnt
 echo "--------------------------------------------------"
 echo -e '\e[32m[4] Mount the file systems\e[39m'
 echo "--------------------------------------------------"
-mount -o noatime,space_cache,compress-force=zstd,subvol=root $DISK3 /mnt
+mount -o noatime,space_cache,compress-force=zstd,subvol=root "$DISK3" /mnt
 mkdir -p /mnt/home
 mkdir -p /mnt/var/cache/pacman
 mkdir -p /mnt/var/log
 mkdir -p /mnt/snapshots
-mount -o noatime,space_cache,compress-force=zstd,subvol=home $DISK3 /mnt/home
-mount -o noatime,space_cache,compress-force=zstd,subvol=pkgs $DISK3 /mnt/var/cache/pacman
-mount -o noatime,space_cache,compress-force=zstd,subvol=logs $DISK3 /mnt/var/log
-mount -o noatime,space_cache,compress-force=zstd,subvol=snapshots $DISK3 /mnt/snapshots
-swapon $DISK2
+mount -o noatime,space_cache,compress-force=zstd,subvol=home "$DISK3" /mnt/home
+mount -o noatime,space_cache,compress-force=zstd,subvol=pkgs "$DISK3" /mnt/var/cache/pacman
+mount -o noatime,space_cache,compress-force=zstd,subvol=logs "$DISK3" /mnt/var/log
+mount -o noatime,space_cache,compress-force=zstd,subvol=snapshots "$DISK3" /mnt/snapshots
+swapon "$DISK2"
 if [ "$BIOS_TYPE" == "uefi" ]; then
 	mkdir -p /mnt/boot/efi
-	mount $DISK1 /mnt/boot/efi
+	mount "$DISK1" /mnt/boot/efi
 fi
 if [ "$BIOS_TYPE" == "bios" ]; then
 	mkdir -p /mnt/boot
-	mount $DISK1 /mnt/boot
+	mount "$DISK1" /mnt/boot
 fi
 
 echo "--------------------------------------------------"
@@ -118,20 +116,20 @@ reflector --age 48 --fastest 5 --latest 10 --protocol https --sort rate --save /
 echo "--------------------------------------------------"
 echo -e '\e[32m[6] Install essential packages\e[39m'
 echo "--------------------------------------------------"
-sed -i 's/#Color/Color\nILoveCandy/' /etc/pacman.conf
-sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+sed -i "s/#Color/Color\nILoveCandy/" /etc/pacman.conf
+sed -i "s/#ParallelDownloads/ParallelDownloads/" /etc/pacman.conf
 pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware pacman-contrib
-sed -i 's/#Color/Color\nILoveCandy/' /mnt/etc/pacman.conf
-sed -i 's/#ParallelDownloads/ParallelDownloads/' /mnt/etc/pacman.conf
+sed -i "s/#Color/Color\nILoveCandy/" /mnt/etc/pacman.conf
+sed -i "s/#ParallelDownloads/ParallelDownloads/" /mnt/etc/pacman.conf
 
 echo "--------------------------------------------------"
 echo -e '\e[32m[7] Microcode\e[39m'
 echo "--------------------------------------------------"
 CPU=$(grep vendor_id /proc/cpuinfo)
-if [[ $CPU == *"AuthenticAMD"* ]]; then
+if [[ "$CPU" == *"AuthenticAMD"* ]]; then
 	arch-chroot /mnt pacman -S amd-ucode --noconfirm
 fi
-if [[ $CPU == *"GenuineIntel"* ]]; then
+if [[ "$CPU" == *"GenuineIntel"* ]]; then
 	arch-chroot /mnt pacman -S intel-ucode --noconfirm
 fi
 
@@ -143,28 +141,28 @@ genfstab -U /mnt >>/mnt/etc/fstab
 echo "--------------------------------------------------"
 echo -e '\e[32m[9] Set the time zone\e[39m'
 echo "--------------------------------------------------"
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/$(curl -s http://ip-api.com/line?fields=timezone) /etc/localtime
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/"$(curl -s http://ip-api.com/line?fields=timezone)" /etc/localtime
 arch-chroot /mnt hwclock --systohc
 
 echo "--------------------------------------------------"
 echo -e '\e[32m[10] Localization\e[39m'
 echo "--------------------------------------------------"
-arch-chroot /mnt sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+arch-chroot /mnt sed -i "s/#en_US.UTF-8/en_US.UTF-8/" /etc/locale.gen
 arch-chroot /mnt locale-gen
 arch-chroot /mnt localectl set-locale LANG=en_US.UTF-8
-arch-chroot /mnt echo 'export LANG=en_US.UTF-8' | tee -a /etc/profile
+arch-chroot /mnt echo "export LANG=en_US.UTF-8" | tee -a /etc/profile
 arch-chroot /mnt localectl status
 
 echo "--------------------------------------------------"
 echo -e '\e[32m[11] Network configuration\e[39m'
 echo "--------------------------------------------------"
 echo -e '\e[33mSet hostname:\e[39m'
-read HOSTNAME
-arch-chroot /mnt echo $HOSTNAME >/etc/hostname
+read -r HOSTNAME
+arch-chroot /mnt echo "$HOSTNAME" >/etc/hostname
 arch-chroot /mnt cat >/etc/hosts <<EOF
 127.0.0.1   localhost
 ::1         localhost
-127.0.1.1   $hostname.localdomain   $hostname
+127.0.1.1   $HOSTNAME.localdomain   $HOSTNAME
 EOF
 
 echo "--------------------------------------------------"
@@ -172,11 +170,11 @@ echo -e '\e[32m[12] Add user\e[39m'
 echo "--------------------------------------------------"
 arch-chroot /mnt pacman -S fish --noconfirm
 echo -e '\e[33mSet username:\e[39m'
-read USERNAME
-arch-chroot /mnt useradd -m -G wheel -s /bin/fish $USERNAME
+read -r USERNAME
+arch-chroot /mnt useradd -m -G wheel -s /bin/fish "$USERNAME"
 echo -e '\e[33mSet user password:\e[39m'
-arch-chroot /mnt passwd $USERNAME
-arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+arch-chroot /mnt passwd "$USERNAME"
+arch-chroot /mnt sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers
 arch-chroot /mnt pacman -S xdg-user-dirs --noconfirm
 arch-chroot /mnt xdg-user-dirs-update
 echo -e '\e[33mSet root password:\e[39m'
@@ -191,7 +189,7 @@ if [ -d /sys/firmware/efi ]; then
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 else
 	arch-chroot /mnt pacman -S btrfs-progs grub grub-btrfs --noconfirm
-	arch-chroot /mnt grub-install --target=i386-pc $DISK
+	arch-chroot /mnt grub-install --target=i386-pc "$DISK"
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 fi
 arch-chroot /mnt cat >/usr/share/libalpm/hooks/grub-update-after-kernel.hook <<EOF
@@ -218,8 +216,8 @@ EOF
 echo "--------------------------------------------------"
 echo -e '\e[32m[14] Update packages\e[39m'
 echo "--------------------------------------------------"
-arch-chroot /mnt sed -i 's/#[multilib]/[multilib]/' /etc/pacman.conf
-arch-chroot /mnt sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/' /etc/pacman.conf
+arch-chroot /mnt sed -i "s/#[multilib]/[multilib]/" /etc/pacman.conf
+arch-chroot /mnt sed -i "s/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlist/" /etc/pacman.conf
 arch-chroot /mnt pacman -S reflector --noconfirm
 arch-chroot /mnt reflector --age 48 --fastest 5 --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 arch-chroot /mnt pacman -Syu
@@ -227,15 +225,15 @@ arch-chroot /mnt pacman -Syu
 echo "--------------------------------------------------"
 echo -e '\e[32m[15] Environment\e[39m'
 echo "--------------------------------------------------"
-arch-chroot /mnt echo 'XAUTHORITY=${XDG_RUNTIME_DIR}/Xauthority' | tee -a /etc/environment
+arch-chroot /mnt echo "XAUTHORITY=${XDG_RUNTIME_DIR}/Xauthority" | tee -a /etc/environment
 
 echo "--------------------------------------------------"
 echo -e '\e[32m[16] Optimization\e[39m'
 echo "--------------------------------------------------"
-arch-chroot /mnt echo 'vm.swappiness=10' > /etc/sysctl.d/99-swappiness.conf
-arch-chroot /mnt sed -i 's/#MAKEFLAGS/MAKEFLAGS/' /etc/makepkg.conf
-arch-chroot /mnt sed -i 's/-j2/-j$(nproc)/' /etc/makepkg.conf
-arch-chroot /mnt sed -i 's/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+arch-chroot /mnt echo "vm.swappiness=10" >/etc/sysctl.d/99-swappiness.conf
+arch-chroot /mnt sed -i "s/#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+arch-chroot /mnt sed -i "s/-j2/-j$(nproc)/" /etc/makepkg.conf
+arch-chroot /mnt sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
 arch-chroot /mnt pacman -S ufw gufw --noconfirm
 arch-chroot /mnt systemctl enable ufw.service
 arch-chroot /mnt ufw enable
@@ -291,13 +289,13 @@ echo -e '\e[32m[23] GNOME\e[39m'
 echo "--------------------------------------------------"
 arch-chroot /mnt pacman -S eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-control-center nautilus python-nautilus gnome-screenshot gnome-shell-extensions gnome-system-monitor gnome-terminal --noconfirm
 arch-chroot /mnt systemctl enable gdm.service
-arch-chroot /mnt sed -i 's/#WaylandEnable=false/WaylandEnable=false/' /etc/gdm/custom.conf
+arch-chroot /mnt sed -i "s/#WaylandEnable=false/WaylandEnable=false/" /etc/gdm/custom.conf
 
 echo "--------------------------------------------------"
 echo -e '\e[32m[24] Extra\e[39m'
 echo "--------------------------------------------------"
 arch-chroot /mnt pacman -S neovim git xsel xclip gnome-tweaks dconf-editor webp-pixbuf-loader p7zip unrar gvfs-gphoto2 gvfs-mtp sushi xdg-user-dirs-gtk --noconfirm
-arch-chroot /mnt sed -i 's/%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+arch-chroot /mnt sed -i "s/%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
 
 echo "--------------------------------------------------"
 echo -e '\e[32m[25] Remove install script\e[39m'
